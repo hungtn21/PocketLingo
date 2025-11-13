5f1d9c4c4387c17b2d07f92729988444f0fe123c
huyen <huyendnk1808@gmail.com>
2025-11-14 01:22:47 +0700

    setup frontend

---
+++ frontend/src/pages/User/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className="profile-page" style={{ padding: '1rem', textAlign: 'center' }}>
+			<h1>Profile</h1>
+			<p>Simple placeholder for user profile.</p>
+		</div>
+	);
+};
+
+export default Profile;
+
